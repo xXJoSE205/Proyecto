0897eedeeaf8958d5f44f73eb88b48b55cc35933
Jose Antonio Muñoz <37207639+xXJoSE205@users.noreply.github.com>
2018-03-20 10:33:57 +0100

    ni p idea hulio

--- src/p3/Opinion.java
+++ src/p3/Opinion.java
@@ -6,12 +6,27 @@
 package p3;
 
 public abstract class Opinion {
+    /** Autor de la opinion*/
     private Demandante autor;
 
+    /**
+     * Constructor de Opinion
+     *
+     * @param autor Autor del comentario
+     * @throws NullPointerException si el autor es null
+     */
     public Opinion(Demandante autor) {
+        if(autor==null){
+            throw new NullPointerException("Autor null");
+        }
         this.autor = autor;
     }
 
+    /**
+     * Devuelve el autor de la opinion
+     *
+     * @return Demandante, autor de la opinion
+     */
     public Demandante getAutor() {
         return autor;
     }
