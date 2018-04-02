cca27069efb7b8164b0f4e275659f86a6d176a77
jrgmat98 <jorgematseg98@gmail.com>
2018-04-02 21:45:11 +0200

    Haber si me muero, traps are not gay,Astolfo

--- src/p3/pruebas/UsuarioRegistradoTest.java
+++ src/p3/pruebas/UsuarioRegistradoTest.java
@@ -8,8 +8,18 @@ import static org.junit.Assert.*;
 
 public class UsuarioRegistradoTest {
 
+    /**
+     * Cliente general para realizar el test
+     * Se instanciara como Demandante, clase descendiente de Cliente, clase descendiente de UsuarioRegistradi
+     * La clase UsuarioRegistrado es abstracta y no es instanciable
+     */
+
     private Demandante demandante;
 
+    /**
+     * Crea un UsuarioRegistrado como demandante para los test
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
     demandante = new Demandante("Tony","Stark","12345678Q","Contraseña",
@@ -17,36 +27,55 @@ public class UsuarioRegistradoTest {
 
     }
 
+    /**
+     * Test de getNombre, se comprueba que el nombre sea igual a "Tony"
+     */
+
     @Test
     public void getNombre() {
         System.out.println("UsuarioRegistrado: test getNombre");
         assertTrue(demandante.getNombre()=="Tony");
     }
 
+    /**
+     * Test de getApellidos, se comprueba que el apellido sea igual a "Stark"
+     */
     @Test
     public void getApellidos() {
         System.out.println("UsuarioRegistrado: test getApellidos");
         assertTrue(demandante.getApellidos()=="Stark");
     }
 
+    /**
+     * Test de getNif, se comprueba que el NIF sea igual a "12345678Q"
+     */
     @Test
     public void getNif() {
         System.out.println("UsuarioRegistrado: test getNif");
         assertTrue(demandante.getNif()=="12345678Q");
     }
 
+    /**
+     * Test de getPassword, se comprueba que la contraseña sea igual a "Contraseña"
+     */
     @Test
     public void getPassword() {
         System.out.println("UsuarioRegistrado: test getPassword");
         assertTrue(demandante.getPassword()=="Contraseña");
     }
 
+    /**
+     * Test de isLogeado, se comprueba que el UsuarioRegistrado del setUp no esta logeado
+     */
     @Test
     public void isLogeado() {
         System.out.println("UsuarioRegistrado: test isLogeado");
         assertFalse(demandante.isLogeado());
     }
 
+    /**
+     * Test de setLogeado, se comprueba que el UsuarioRegistrado ahora esta logeado
+     */
     @Test
     public void setLogeado() {
         System.out.println("UsuarioRegistrado: test setLogeado");
--- src/p3/pruebas/ValoracionTest.java
+++ src/p3/pruebas/ValoracionTest.java
@@ -9,8 +9,19 @@ import static org.junit.Assert.*;
 
 public class ValoracionTest {
 
+    /**
+     * Valoracion general para el test
+     */
     private Valoracion valoracion;
+    /**
+     * Demandante general para el test
+     */
     private Demandante demandante;
+
+    /**
+     * Crea una valoracion para el test
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
          demandante = new Demandante("Tony","Stark","12345678Q","Contraseña",
@@ -18,24 +29,38 @@ public class ValoracionTest {
         valoracion = new Valoracion(demandante, 5);
     }
 
+    /**
+     * Test 1 del constructor, se espera un IllegalArgumentException
+     * La puntuacion es mayor que 5
+     */
     @Test (expected = IllegalArgumentException.class)
-    public void constructor2(){
-        System.out.println("Valoracion: test constructor2");
+    public void constructor1(){
+        System.out.println("Valoracion: test constructor1");
         Valoracion valoracion = new Valoracion(demandante, 7);
     }
 
+    /**
+     * Test 2 del constructor, se espera un NullPointerException
+     * El autor es NULL
+     */
     @Test (expected = NullPointerException.class)
-    public void constructor3(){
-        System.out.println("Valoracion: test constructor3");
+    public void constructor2(){
+        System.out.println("Valoracion: test constructor2");
         Valoracion valoracion = new Valoracion(null, 5);
     }
-    
+
+    /**
+     * Test de getPuntuacion, se comprueba que la puntuacion es igual a 5
+     */
     @Test
     public void getPuntuacion() {
         System.out.println("Valoracion: test getPuntuacion");
         assertTrue(valoracion.getPuntuacion()==5);
     }
 
+    /**
+     * Test de setPuntuacion1, se comprueba que la nueva puntuacion es igual a 4
+     */
     @Test
     public void setPuntuacion1() {
         System.out.println("Valoracion: test setPuntuacion1");
@@ -43,6 +68,10 @@ public class ValoracionTest {
         assertTrue(valoracion.getPuntuacion()==4);
     }
 
+    /**
+     * Test de setPuntuacion2, se espera IllegalArgumentException
+     * La puntuacion es mayor que 5
+     */
     @Test (expected = IllegalArgumentException.class)
     public void setPuntuacion2(){
         System.out.println("Valoracion: test setPuntuacion2");
