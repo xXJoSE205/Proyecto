a7d0129381f5f0c45b2535bda01a11000e399e8d
jrgmat98 <jorgematseg98@gmail.com>
2018-04-06 18:39:35 +0200

    Haber si me muero, traps are not gay,Astolfo

--- src/p3/src/Estado.java
+++ src/p3/src/Estado.java
@@ -1,4 +1,10 @@
+/**
+ * Esta enumeracion contiene los posibles estados de una oferta
+ *
+ * @author Jorge Mateo Segura y José Antonio Muñoz Ortega
+ */
 package p3.src;
+
 
 import java.io.Serializable;
 
