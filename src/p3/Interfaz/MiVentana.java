3d93f671a0d7d45bc712bc4f2d34975f89b75965
jrgmat98 <jorgematseg98@gmail.com>
2018-04-10 10:46:45 +0200

    Haber si me muero, traps are not gay,Astolfo

--- src/p3/Interfaz/MiVentana.java
+++ src/p3/Interfaz/MiVentana.java
@@ -1,4 +1,26 @@
 package p3.Interfaz;
 
-public class MiVentana {
+import javax.swing.*;
+import java.awt.*;
+
+public class MiVentana extends JFrame  {
+
+    public MiVentana() {
+        super("Muzska");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Image icon = new ImageIcon(("definitivo.png")).getImage();
+        setIconImage(icon);
+
+        setSize(350, 350);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MiVentana();
+    }
+
+
 }
